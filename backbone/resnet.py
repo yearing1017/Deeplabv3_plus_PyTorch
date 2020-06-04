74c3cf72d41aa379c40ce50b7103a208e4f4f963
yearing1017 <33566006+yearing1017@users.noreply.github.com>
2020-06-04 10:34:25 +0800

    Update resnet.py

--- backbone/resnet.py
+++ backbone/resnet.py
@@ -2,7 +2,6 @@ import math
 import torch
 import torch.nn as nn
 import torch.utils.model_zoo as model_zoo
-from batchnorm import SynchronizedBatchNorm2d
 
 class Bottleneck(nn.Module):
     expansion = 4
@@ -129,9 +128,6 @@ class ResNet(nn.Module):
             if isinstance(m, nn.Conv2d):
                 n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
                 m.weight.data.normal_(0, math.sqrt(2. / n))
-            elif isinstance(m, SynchronizedBatchNorm2d):
-                m.weight.data.fill_(1)
-                m.bias.data.zero_()
             elif isinstance(m, nn.BatchNorm2d):
                 m.weight.data.fill_(1)
                 m.bias.data.zero_()
