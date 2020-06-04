154a5ba85af71623475731a02e3a99a5ec4198dd
yearing1017 <33566006+yearing1017@users.noreply.github.com>
2020-06-04 10:29:28 +0800

    Create build_bk.py

---
+++ build_bk.py
@@ -0,0 +1,9 @@
+from backbone import resnet, xception
+
+def build_backbone(backbone, output_stride, BatchNorm):
+    if backbone == 'resnet':
+        return resnet.ResNet101(output_stride, BatchNorm)
+    elif backbone == 'xception':
+        return xception.AlignedXception(output_stride, BatchNorm)
+    else:
+        raise NotImplementedError
